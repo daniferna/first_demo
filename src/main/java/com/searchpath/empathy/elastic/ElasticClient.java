dc2089d1ab6e63f3a8139dd394b0167c0edffb78
Daniel Fernandez Aller <danielfa@empathy.co>
2021-02-26 09:41:58 +0100

    Close Elastic connection when context closes

--- src/main/java/com/searchpath/empathy/elastic/ElasticClient.java
+++ src/main/java/com/searchpath/empathy/elastic/ElasticClient.java
@@ -4,7 +4,9 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
+import javax.annotation.PreDestroy;
 import javax.inject.Singleton;
+import java.io.IOException;
 
 /**
  * Class implementing the Singleton design pattern using @Singleton to access the ElasticSearch client
@@ -13,11 +15,24 @@ import javax.inject.Singleton;
 @Singleton
 public class ElasticClient {
 
-    private RestHighLevelClient client = new RestHighLevelClient(
+    private final RestHighLevelClient client = new RestHighLevelClient(
             RestClient.builder(
                     new HttpHost("localhost", 9200, "http"),
                     new HttpHost("localhost", 9300, "http"))
-    );;
+    );
+
+    /**
+     * Method to be called when the context is closed.
+     * It closes the pool connection created by the RestHighLevelClient
+     */
+    @PreDestroy
+    private void close() {
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public RestHighLevelClient getClient() {
         return client;
