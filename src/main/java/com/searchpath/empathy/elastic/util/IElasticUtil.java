44d242c679dbf0968af02fad8a1187c4e897366a
Daniel Fernandez Aller <danielfa@empathy.co>
2021-03-03 13:49:59 +0100

    Completed and formatted all documentation of IElasticUtil and ElasticClientUtil.

--- src/main/java/com/searchpath/empathy/elastic/util/IElasticUtil.java
+++ src/main/java/com/searchpath/empathy/elastic/util/IElasticUtil.java
@@ -9,18 +9,28 @@ public interface IElasticUtil {
 
     /**
      * Method that calls the ElasticSearch client to get the cluster name.
-     * @see ElasticClient#getClient()
+     *
      * @return The cluster name of the elastic search
      * @throws IOException If it can't access the elastic search
+     * @see ElasticClient#getClient()
      */
     String getClusterName() throws IOException;
 
     /**
      * This method reads the file containing the IMDB films and index them into elastic search.
      * All reading and processing operations shall be lazy in order to save memory.
+     *
      * @return "Success loading data" if everything went well.
      */
     String loadIMDBData() throws IOException;
 
+    /**
+     * Search films by their title and transform the result of the query into a QueryResponse object.
+     *
+     * @param title The title of the movie you are looking for
+     * @return A QueryResponse object containing the films and the number of the results
+     * @throws IOException In case there is an error accessing elastic search.
+     * @see QueryResponse
+     */
     QueryResponse searchFilmsByTitle(String title) throws IOException;
 }
--- src/main/java/com/searchpath/empathy/elastic/util/impl/ElasticClientUtil.java
+++ src/main/java/com/searchpath/empathy/elastic/util/impl/ElasticClientUtil.java
@@ -15,7 +15,6 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.core.MainResponse;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
@@ -38,6 +37,7 @@ public class ElasticClientUtil implements IElasticUtil {
     @Inject
     ObjectMapper objectMapper;
 
+
     public String getClusterName() throws IOException {
         MainResponse response = null;
         response = client.getClient().info(RequestOptions.DEFAULT);
@@ -45,17 +45,16 @@ public class ElasticClientUtil implements IElasticUtil {
     }
 
     /**
-     * This implementation reads the file
-     * @see #readFile(InputStream)
-     * and then, it divides the entry into chunks of 50.000 lines with the help of the Guava's Iterators library help.
-     * @see Iterators#partition(Iterator, int)
-     * Following that, read each chunk and proceeds to create a
-     * @see Film
-     * object which is then deserialized into a Json and added to a
-     * @see BulkRequest
-     * Next step is take the elastic search client and upload this bulk. And repeat this for every chunk.
-     * @throws IOException If the method can't deserialize the film object into JSON or an error occur while loading
-     * the bulk data through the client.
+     * This implementation reads the file {@see #readFile(InputStream)} and then, it divides the entry into chunks
+     * of 50.000 lines with the help of the Guava's Iterators library help. {@see Iterators#partition(Iterator, int)}
+     * <p>
+     * Following that, read each chunk and proceeds to create a {@link Film} object which is then deserialized into
+     * a Json and added to a {@link BulkRequest}. Next step is take the elastic search client and upload this bulk.
+     * <p>
+     * This is repeat for every chunk.
+     *
+     * @throws IOException If the method can't deserialize the film object into JSON
+     *                     or an error occur while loading the bulk data through the client.
      */
     public String loadIMDBData() throws IOException {
         var reader = readFile(this.getClass().getClassLoader().getResourceAsStream("data.tsv"));
@@ -83,6 +82,16 @@ public class ElasticClientUtil implements IElasticUtil {
         return "Success loading data";
     }
 
+    /**
+     * This implementation use the {@link ElasticClient} API to look for the films.
+     * It creates a {@link SearchRequest}, and using a {@link SearchSourceBuilder}, creates the query.
+     * <p>
+     * Once the response is obtained, is processed by a private helper method {@see #parseHitToFilms(SearchHits)}
+     * which transform the hits into {@link Film} objects.
+     *
+     * @throws IOException If the method can't serialize the hit into a Film JSON or an error occur while searching
+     *                     the query through the client.
+     */
     @Override
     public QueryResponse searchFilmsByTitle(String title) throws IOException {
         var request = new SearchRequest("imdb");
@@ -98,6 +107,12 @@ public class ElasticClientUtil implements IElasticUtil {
         return new QueryResponse(total, films);
     }
 
+    /**
+     * Helper method which transforms the elastic search response hits into Film objects.
+     *
+     * @param hits The hits of the search
+     * @return An array of films {@see Film}
+     */
     private Film[] parseHitToFilms(SearchHits hits) {
         return Arrays.stream(hits.getHits()).map(hit -> {
             String source = hit.getSourceAsString();
@@ -110,10 +125,8 @@ public class ElasticClientUtil implements IElasticUtil {
         }).toArray(Film[]::new);
     }
 
-
-
     /**
-     * Helper method, takes an InputStream and return a BufferedReader
+     * Helper method, takes an {@link InputStream} and return a {@link BufferedReader}
      *
      * @param dataPath The InputStream pointing to the file
      * @return The BufferedReader of the file
