c61236bc46c55c085cac19ff2122a9c5ebec686c
Daniel Fernandez Aller <danielfa@empathy.co>
2021-03-10 13:53:41 +0100

    Fixed POJO not deserializing.

--- src/main/java/com/searchpath/empathy/POJO/Film.java
+++ src/main/java/com/searchpath/empathy/POJO/Film.java
@@ -1,28 +1,24 @@
 package com.searchpath.empathy.pojo;
 
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.util.Arrays;
 import java.util.StringJoiner;
 
+@JsonNaming(value=PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Film {
 
-    @JsonProperty("id")
-    private final String id;
-    @JsonProperty("title")
-    private final String title;
-    @JsonProperty("genres")
-    private final String[] genres;
-    @JsonProperty("type")
-    private final String type;
-    @JsonProperty("start_year")
-    private final String start_year;
-    @JsonProperty("end_year")
-    private final String end_year;
+    private String id;
+    private String title;
+    private String[] genres;
+    private String type;
+    private String start_year;
+    private String end_year;
 
-    @JsonCreator
+    public Film() {}
+
     public Film(String id, String title, String[] genres, String type, String start_year, String end_year) {
         this.id = id;
         this.title = title;
@@ -63,8 +59,8 @@ public class Film {
                 .add("title='" + title + "'")
                 .add("genres=" + Arrays.toString(genres))
                 .add("type='" + type + "'")
-                .add("startDate='" + start_year + "'")
-                .add("endDate='" + end_year + "'")
+                .add("start_year=" + start_year)
+                .add("end_year=" + end_year)
                 .toString();
     }
 }
